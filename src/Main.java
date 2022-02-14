ea983a9f950b2b703a075a11da7b92ccb6a01e0b
unknown <anhnt.tb12@gmail.com>
2022-02-15 00:04:39 +0700

    lack move()

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point p1 = new Point(10,5);
+        MovablePoint m1 = new MovablePoint(10,5,22,11);
+        System.out.println(m1.toString());
+    }
+
+
+}
